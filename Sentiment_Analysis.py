3951dac8014288181906786f8d9895a2015a5dec
keremyalintaskin <62906984+keremyalintaskin@users.noreply.github.com>
2025-03-02 16:42:12 +0300

    Update Sentiment_Analysis.py

--- Sentiment_Analysis.py
+++ Sentiment_Analysis.py
@@ -2,20 +2,20 @@ import praw
 from textblob import TextBlob
 import matplotlib.pyplot as plt
 
-# Reddit API'ye bağlanma
+
 reddit = praw.Reddit(
     client_id="jPWcyiuijkgzftreVsICMA",
     client_secret="x51QzHrXoPAOI8XfPW21z_5fp79C7g",
     user_agent="VibeCodingScraper/1.0 (by u/Far-Strength-2949)"
 )
 
-# "Modern Yazılım Geliştirmede Yapay Zeka" başlığıyla arama yap
+
 subreddit = reddit.subreddit("all")
 posts = []
 for post in subreddit.search("Modern Yazılım Geliştirmede Yapay Zeka", limit=1500):
     posts.append(post.title)
 
-# Sentiment analizi yapalım
+
 pozitif = 0
 negatif = 0
 nötr = 0
@@ -30,12 +30,12 @@ for post in posts:
     else:
         nötr += 1
 
-# Sonuçları görselleştirelim
+
 etiketler = 'Pozitif', 'Negatif', 'Nötr'
 boyutlar = [pozitif, negatif, nötr]
 renkler = ['gold', 'lightcoral', 'lightskyblue']
 
 plt.pie(boyutlar, labels=etiketler, colors=renkler, autopct='%1.1f%%', startangle=140)
-plt.axis('equal')  # Eşit dağılımda çizim
+plt.axis('equal') 
 plt.title("Yazılım Geliştirmede Yapay Zeka Konusunda Reddit Gönderilerinin Sentiment Analizi")
 plt.show()
